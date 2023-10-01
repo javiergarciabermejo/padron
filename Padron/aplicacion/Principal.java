b2deffd2fb136c41d0e43fa217aaa7c54e395543
javiergarciabermejo <145556743+javiergarciabermejo@users.noreply.github.com>
2023-10-01 22:40:30 +0200

    Add files via upload

---
+++ Padron/aplicacion/Principal.java
@@ -0,0 +1,17 @@
+package aplicacion;
+
+import presentacion.*;
+
+public class Principal
+{ public static void main(String[] args){
+    InterfazUsuario.ejecutar(args);
+}
+}
+
+/**
+ * Write a description of class Principal here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+ 
